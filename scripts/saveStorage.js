e5f6bbdb2dd1d6ffbeb86c5bf508eeb5052a8cee
juantuberquia <tuberquia26@hotmail.com>
2022-09-27 17:46:22 -0500

    get data local storage and show them 2

--- scripts/saveStorage.js
+++ scripts/saveStorage.js
@@ -5,31 +5,3 @@ const saveStorage = (dataUser) => {
     JSON.stringify(dataUser[sizeCollection - 1])
   );
 };
-
-// window.addEventListener("load", getDataStorage());
-
-// function getDataStorage() {
-//   for (let i = 0; i < localStorage.length; i++) {
-//     let clave = localStorage.key(i);
-//     console.log("Clave: " + clave);
-//     let getDataLocal = JSON.parse(localStorage.getItem(clave));
-//     console.log(getDataLocal.nombre);
-
-//     let summary = document.getElementById("col-summary-quote");
-//     let containerQuote = document.createElement("div");
-
-//     let paragraphName = document.createElement("p");
-//     paragraphName.innerText = getDataLocal.nombre;
-
-//     let paragraphType = document.createElement("p");
-//     paragraphType.innerText = getDataLocal.tipoCorte;
-
-//     let paragraphTime = document.createElement("p");
-//     paragraphTime.innerText = getDataLocal.fecha;
-
-//     containerQuote.appendChild(paragraphName);
-//     containerQuote.appendChild(paragraphType);
-//     containerQuote.appendChild(paragraphTime);
-//     summary.appendChild(containerQuote);
-//   }
-// }
